99c0de7df8764911e9b61cd872d50ef418b6601b
Arnabya Agrawal <arnabyagrawal15@gmail.com>
2022-04-24 14:09:02 +0530

    PPS CLA 1

---
+++ CLA1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+ int main()
+ {
+     int a,b,c,d;
+     scanf("%d\n%d\n%d\n%d",&a,&b,&c,&d);
+     printf("%d",a+b+c+d);
+     printf("\n%d",(a-b)-(c-d));
+     printf("\n%d",(a*b)*(c*d));
+     
+ }
